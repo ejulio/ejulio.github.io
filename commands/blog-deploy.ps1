f1bc315b7547e1df7645d4bfede88363775528bf
Júlio César Batista <jcb.1611@gmail.com>
2015-07-21 20:38:52 -0300

    correção no deploy

--- commands/blog-deploy.ps1
+++ commands/blog-deploy.ps1
@@ -1,12 +1,18 @@
+param([string]$mensagem)
 cd source
 
 git add --all
+git commit -m "$mensagem"
 git push origin source
 
 hexo generate --deploy
 
+copy CNAME ../master
+copy .nojekyll ../master
+
 cd ../master
 git add --all
+git commit -m "$mensagem"
 git push origin master
 
 cd ../
